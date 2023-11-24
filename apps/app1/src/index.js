60c267ab1ff7eb4b9a9c1ae203b0794e69c5372b
Antonio Diogo C. Franco <adfranco@stefanini.com>
2023-11-23 23:06:43 -0300

    add images

--- apps/app1/src/index.js
+++ apps/app1/src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer()
 
 server.on("request", (request, response) => {
+    console.log("API1 :: Request");
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
     const responseBody = {
@@ -15,10 +16,11 @@ server.on("request", (request, response) => {
     response.end();
 })
 
-server.listen(3001, error => {
+const port = 3001
+server.listen(port, error => {
     if (error) {
         return console.log(error);
     }
 
-    console.log("server running");
+    console.log("server running", port);
 });
