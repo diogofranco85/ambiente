ae34f58b95cbd9cd1001bc4355cef0fef3772647
Antonio Diogo C. Franco <adfranco@stefanini.com>
2023-11-23 22:29:59 -0300

    initial commit

---
+++ app/health/src/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer()
+
+server.on("request", (request, response) => {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+    const responseBody = {
+        success: true,
+        dateTime: (new Date()).toISOString()
+    }
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+})
+
+server.listen(3001, error => {
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log("server running");
+});
